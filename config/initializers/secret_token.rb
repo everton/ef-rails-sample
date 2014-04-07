88bc64b090a1ef8efb585144c00d6827387839d8
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-06 23:00:04 -0300

    Initial import: Rails new
      Ruby version              2.1.1 (i686-linux)
      RubyGems version          2.2.2
      Rack version              1.5
      Rails version             4.0.4
      JavaScript Runtime        Node.js (V8)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoPublicationExample::Application.config.secret_key_base = '121a00283a3fed72b1a8276bac5a3c23843c739a92989fe03918f841a8172812f7e5d96e84225616a953f6350c7e53b022a6aa200058db839faa6e25b685023f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VideoPublicationExample::Application.config.session_store :cookie_store, key: '_video-publication-example_session'
