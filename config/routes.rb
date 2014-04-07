227d2eac840348b0f261ae202b2e35b7003dee47
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-07 00:29:22 -0300

    Add Resque server app at '/resque' URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'resque/server'
+
 VideoPublicationExample::Application.routes.draw do
+  mount Resque::Server.new, at: "/resque"
+
   resources :videos
 
   # The priority is based upon order of creation: first created -> highest priority.
