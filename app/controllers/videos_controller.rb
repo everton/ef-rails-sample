8f7465a2b9e139d7277bbbd6144f81c1773d460b
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-07 00:30:12 -0300

    Add #index, #show, #new and #create actions for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,33 @@
 class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+  end
+
+  def show
+    @video = Video.find params[:id]
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+
+    respond_to do |format|
+      if @video.save
+        format.html do
+          redirect_to @video,
+            notice: 'Video was successfully created.'
+        end
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:title, :description, :video)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def main_thumb_of(video)
+    asset_path "videos/#{Rails.env}/#{video.id}/thumbs/thumb-1.jpg"
+  end
+
+  def thumbs_of(video)
+    thumbs_path = Rails.root
+      .join "public/videos/#{Rails.env}/#{video.id}/thumbs/*.jpg"
+
+    Dir[thumbs_path].each.map do |thumb|
+      path = thumb.gsub Rails.root.join('public').to_s, ''
+
+      asset_path(path)
+    end
+  end
+
+  def video_path_of(video, format)
+    id = video.id
+    asset_path "videos/#{Rails.env}/#{id}/#{id}.#{format}"
+  end
 end
