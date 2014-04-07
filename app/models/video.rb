33d2e765f5cae398c02dd5f2f3b0079604ecd6ec
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-06 23:44:15 -0300

    Apply Resque enqueue of Video processors to 'Video#after_save'

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,14 @@
 class Video < ActiveRecord::Base
+  attr_accessor :video
+
+  after_save :process_video
+
+  private
+  def process_video
+    return unless video
+
+    Resque.enqueue(ThumbsProcessor, self.id, video.path)
+    Resque.enqueue(   Mp4Processor, self.id, video.path)
+    Resque.enqueue(   OggProcessor, self.id, video.path)
+  end
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,33 @@
+require 'fileutils'
+
 require 'test_helper'
 
 class VideoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionDispatch::TestProcess
+
+  setup do
+    @inline  = Resque.inline
+    Resque.inline = true
+  end
+
+  teardown do
+    Resque.inline = @inline
+    FileUtils.rm_rf Rails.root.join 'public/videos/test/'
+  end
+
+  test 'video creation' do
+    video_file =
+      fixture_file_upload 'videos/2001-a-space-odyssey-teaser.mp4'
+
+    assert_difference ->{ Video.count } do
+      video = Video.create title: 'Lorem Ipsum', video: video_file
+
+      videos_path = Rails.root.join "public/videos/test/#{video.id}"
+
+      assert File.exist? videos_path.join "#{video.id}.mp4"
+      assert File.exist? videos_path.join "#{video.id}.ogg"
+
+      assert File.exist? videos_path.join "thumbs/thumb-1.jpg"
+    end
+  end
 end
