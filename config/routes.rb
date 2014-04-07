89c67b476b6f25284280c31ffb43c0605138be16
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-06 23:48:14 -0300

    Add videos resource (rails generate)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 VideoPublicationExample::Application.routes.draw do
+  resources :videos
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
