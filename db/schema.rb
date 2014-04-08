3465b2dd8c15b9d4a0b04665c8dc92cf761bfa74
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-07 22:35:26 -0300

    Add schema updated by users migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407020218) do
+ActiveRecord::Schema.define(version: 20140408013415) do
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "videos", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "videos", ["user_id"], name: "index_videos_on_user_id"
 
 end
