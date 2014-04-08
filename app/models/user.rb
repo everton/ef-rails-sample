c6fd9f9582852d4fc8516a2fbc10a9cce89b2de4
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-07 23:30:57 -0300

    Add some validations for users attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email,                 presence: true, uniqueness: true
+  validates :password,              presence: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
+
+  has_many :videos, dependent: :destroy
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'password encryption at user creation' do
+    user = User.new email: 'test_user@example.com',
+      password: '123', password_confirmation: '123'
+
+    assert_difference -> { User.count } do
+      user.save
+    end
+
+    assert user.authenticate('123'), 'Password not encrypted properly'
+  end
+
+  test 'password authentication of users already created' do
+    user = users(:george)
+
+    assert user.authenticate('123'),
+      'Can not authenticate user with proper password'
+  end
+
+  test 'destroy existing User and associated Videos' do
+    assert_difference -> { User.count }, -1 do
+      assert_difference -> { Video.count }, -2 do
+        @john.destroy
+      end
+    end
+
+    assert_raises ActiveRecord::RecordNotFound do
+      User.find @john.id
+    end
+  end
+
+  test 'avoid blank email' do
+    assert_bad_value User, :email, '   ', :blank
+  end
+
+  test 'avoid users with duplicated email' do
+    assert_bad_value User, :email, @john.email, :taken
+  end
+
+  test 'avoid blank password' do
+    assert_bad_value User, :password,              '   ', :blank
+    assert_bad_value User, :password_confirmation, '   ', :blank
+  end
+
+  test 'ask for password confirmation' do
+    user = User.new password: '123'
+
+    emsg = error_message_for(:confirmation, attribute: 'Password')
+
+    assert_bad_value user, :password_confirmation, '321', emsg
+  end
 end
